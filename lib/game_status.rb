2b3cd4a36ebadfe8daea8bb5b3075ce44c3fead3
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-21 00:53:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,40 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #First column
+  [1,4,7], #Second column
+  [2,5,8], #Third column
+  [0,4,8], #Diagonal
+  [2,4,6]  #Diagonal 2
+]
+
+def won?(board)
+  win = WIN_COMBINATIONS.select{|win|
+    board.values_at(*win).all?("X") || board.values_at(*win).all?("O")}
+  return win.first
+end
+
+def full?(board)
+  !board.include?(" ")
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  index = won?(board)
+  if index == nil
+    nil
+  else
+    board[index.first]
+  end
+end
